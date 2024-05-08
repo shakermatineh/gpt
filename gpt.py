0c3225fe74a75db538f9a7d83f1ac45d6ae900ef
shakermatineh <shaker.matineh@gmail.com>
2024-05-08 11:17:44 -0700

    back to smaller model to run on cpu

--- gpt.py
+++ gpt.py
@@ -3,18 +3,29 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 3e-4
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
+n_head = 4
+n_layer = 3
+dropout = 0.2
+# ------------
+
+"""
+To scale up change to these:
+batch_size = 64
+block_size = 256
+learning_rate = 1e-3
 n_embd = 384
 n_head = 6
 n_layer = 6
-dropout = 0.2
-# ------------
+
+"""
 
 torch.manual_seed(1337)
 
